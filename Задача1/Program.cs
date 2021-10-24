855e4874dc8791981a257a1c5bec8665fe42fd32
Марина Разумова <marina.razumova.19@yandex.ru>
2021-10-24 19:27:04 +0300

    Добавьте файлы проекта.

---
+++ Задача1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом. 
+             *  Знаки препинания не используются. 
+             *  Найти самое длинное слово в строке.  */
+
+
+            string startString = Console.ReadLine();
+            string[] stringArray = startString.Split();
+            string maxWord = "";
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                if (stringArray[i].Length > maxWord.Length)
+                    maxWord = stringArray[i];
+            }
+
+
+            Console.WriteLine(maxWord);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Задача2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Ввести с клавиатуры предложение. 
+             * Предложение представляет собой слова, разделенные пробелом. 
+             * Знаки препинания не используются. Составить программу, 
+             * определяющую является ли строка палиндромом без учёта пробелов и регистра (пример палиндрома – «А роза упала на лапу Азора»).  */
+
+            string text = Console.ReadLine();
+            text = text.Replace(" ", "");
+            char[] charText = text.ToCharArray();
+            Array.Reverse(charText);
+            string text2 = new string(charText);
+            if (text == text2)
+            {
+                Console.WriteLine("Данная запись является палиндромом.");
+            }
+            if (text != text2)
+            {
+                Console.WriteLine("Данная запись не является палиндромом.");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
